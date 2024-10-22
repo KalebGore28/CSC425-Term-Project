ec6ec0850ddca17783dca3cc551cb26ce1cfd1b5
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2024-10-22 08:53:57 -0500

    push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import React from 'react';
+import JobsList from './components/JobsList';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <JobsList />
+    </div>
+  )
+}
+
+export default App
